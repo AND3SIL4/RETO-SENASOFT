dc8e925eeec1355f122e7d27a8819645fce10712
Gilaufen <145714264+andrealeonSS@users.noreply.github.com>
2023-09-27 19:31:09 -0500

    Revert "FINISHED : ejercicio de deteccion de objetos finalizado (file = detection.js, detection.html)"
    This reverts commit 3091db6d02e1000edc8ce211ebf2650ef0d6fddd.

--- Web app/src/js/detection.js
+++ Web app/src/js/detection.js
@@ -5,89 +5,28 @@
 const PREDICTION_URL = "https://democlasificacionytraduccion.cognitiveservices.azure.com/customvision/v3.0/Prediction/d85a36b3-496f-4041-a115-8223ec0b4452/detect/iterations/modelo%20de%20detecci%C3%B3n%20de%20objetos/url"
 const PREDICTION_KEY = "689e1582511d4a4e87021c582feac9d2";
 
-// captura de imagen perro
-const imgPerro = document.getElementById("imagen-perro");
-const contenedorPerro = document.getElementById("contenedor-perro");
-
-// captura de imagen pato
-const imgPato = document.getElementById('imagen-pato');
-const contendorPato = document.getElementById('contendor-pato');
-
-// captura de imagen persona
-const imgPerson = document.getElementById('imagen-persona');
-const contendorPersona = document.getElementById('contenedor-persona');
-
-// captura de imagen general
-const imgGeneral = document.getElementById('imagen-general');
-const contenedorGeneral = document.getElementById('contenedor-general');
-
+const img = document.getElementById("urlImagen");
 const btn = document.getElementById("boton");
+const imagenContainer = document.getElementById("imagenContainer");
 const title = document.getElementById("title");
 
-let newDogImg; 
-let newDuckImg;
-let newPersonImg;
-let newGeneralImg;
+let newImg; // Declarar la variable newImg en un alcance más amplio
 
-// evento que renderiza la imgen del perro
-imgPerro.addEventListener("input", () => {
-  const imagen = imgPerro.value;
+img.addEventListener("input", () => {
+  const imagen = img.value;
 
   // Crear elemento img
-  newDogImg = document.createElement("img"); // Asignar a la variable newDogImg
-  newDogImg.src = imagen;
-  newDogImg.className = "img-fluid";
+  newImg = document.createElement("img"); // Asignar a la variable newImg
+  newImg.src = imagen;
+  newImg.className = "img-fluid";
 
   // Limpiar contenido
-  contenedorPerro.innerHTML = "";
-  contenedorPerro.appendChild(newDogImg);
+  imagenContainer.innerHTML = "";
+  imagenContainer.appendChild(newImg);
 });
 
-// evento que renderiza la imagen pato
-imgPato.addEventListener("input", () => {
-  const imagen = imgPato.value;
-
-  // Crear elemento img
-  newDuckImg = document.createElement("img"); // Asignar a la variable newDogImg
-  newDuckImg.src = imagen;
-  newDuckImg.className = "img-fluid";
-
-  // Limpiar contenido
-  contendorPato.innerHTML = "";
-  contendorPato.appendChild(newDuckImg);
-});
-
-// evento que renderiza la imagen persona
-imgPerson.addEventListener("input", () => {
-  const imagen = imgPerson.value;
-
-  // Crear elemento img
-  newPersonImg = document.createElement("img"); // Asignar a la variable newDogImg
-  newPersonImg.src = imagen;
-  newPersonImg.className = "img-fluid";
-
-  // Limpiar contenido
-  contendorPersona.innerHTML = "";
-  contendorPersona.appendChild(newPersonImg);
-});
-
-// evento que renderiza la imagen general
-imgGeneral.addEventListener("input", () => {
-  const imagen = imgGeneral.value;
-
-  // Crear elemento img
-  newGeneralImg = document.createElement("img"); // Asignar a la variable newDogImg
-  newGeneralImg.src = imagen;
-  newGeneralImg.className = "img-fluid";
-
-  // Limpiar contenido
-  contenedorGeneral.innerHTML = "";
-  contenedorGeneral.appendChild(newGeneralImg);
-});
-
-
-// Función para consumir la API se le pasan 3 parametros (url de imagen, variable imagen, contenedor)
-async function consume(imagen, newImage, contendor) {
+// Función para consumir la API
+function consume(imagen) {
   const headers = {
     "Prediction-Key": PREDICTION_KEY,
     "Content-Type": "application/json",
@@ -98,9 +37,9 @@ async function consume(imagen, newImage, contendor) {
   console.log("Analizando imagen...");
 
   // Eliminar el canvas anterior antes de procesar la nueva imagen
-  const existingCanvas = contendor.querySelector("canvas");
+  const existingCanvas = imagenContainer.querySelector("canvas");
   if (existingCanvas) {
-    contendor.removeChild(existingCanvas);
+    imagenContainer.removeChild(existingCanvas);
   }
 
   axios
@@ -111,10 +50,10 @@ async function consume(imagen, newImage, contendor) {
 
       // Crear un canvas para mostrar las coordenadas de detección
       const canvas = document.createElement("canvas");
-      canvas.width = newImage.width;
-      canvas.height = newImage.height;
+      canvas.width = newImg.width;
+      canvas.height = newImg.height;
       const context = canvas.getContext("2d");
-      context.drawImage(newImage, 0, 0);
+      context.drawImage(newImg, 0, 0);
 
       // Dibujar las coordenadas de detección en el canvas
       items.forEach((element) => {
@@ -126,8 +65,8 @@ async function consume(imagen, newImage, contendor) {
           // Las coordenadas de detección generalmente se encuentran en element.boundingBox
           const boundingBox = element.boundingBox;
 
-          const originalImgWidth = newImage.naturalWidth;
-          const originalImgHeight = newImage.naturalHeight;
+          const originalImgWidth = newImg.naturalWidth;
+          const originalImgHeight = newImg.naturalHeight;
 
           context.rect(
             boundingBox.left * originalImgWidth,
@@ -171,24 +110,14 @@ async function consume(imagen, newImage, contendor) {
       });
 
       // Limpiar el contenido anterior y agregar la nueva imagen con las coordenadas de detección
-      contendor.innerHTML = '';
-      contendor.appendChild(canvas);
+      imagenContainer.innerHTML = '';
+      imagenContainer.appendChild(canvas);
     })
     .catch((error) => {
       console.error(error);
     });
 }
 
-btn.addEventListener("click", async() => {
-  // funcion para la imagen de perro
-  await consume(imgPerro.value, newDogImg, contenedorPerro); 
-
-  // funcion para la imagen de pato
-  await consume(imgPato.value, newDuckImg, contendorPato);
-
-  // funcion para la imagen de persona
-  await consume(imgPerson.value, newPersonImg, contendorPersona);
-
-  // funcion para la imagen general
-  await consume(imgGeneral.value, newGeneralImg, contenedorGeneral)
+btn.addEventListener("click", () => {
+  consume(img.value);
 });
