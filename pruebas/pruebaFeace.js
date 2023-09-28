224465bf01133466cec58cbbf9198f9cba647565
AND3SIL4 <afsilva6236@misena.edu.co>
2023-09-27 20:00:04 -0500

    UPDATE : inicio de reto de deteccion de rostros

--- pruebas/pruebaFeace.js
+++ pruebas/pruebaFeace.js
@@ -6,9 +6,12 @@ const PREDICTION_URL =
   "https://facesenasofta.cognitiveservices.azure.com/face/v1.0/detect?detectionModel=detection_01";
 const PREDICTION_KEY = "938b8c0614cc4a0c97db14e29dd3cc00";
 
+// captura de primera imagen de rostro
 const img = document.getElementById("urlImagen");
-const btn = document.getElementById("boton");
 const imagenContainer = document.getElementById("imagenContainer");
+
+
+const btn = document.getElementById("boton");
 const title = document.getElementById("title");
 
 let newImg; // Declarar la variable newImg en un alcance más amplio
@@ -52,71 +55,72 @@ function consume(imagen) {
       console.log(result);
 
 
-      // // Crear un canvas para mostrar las coordenadas de detección
-      // const canvas = document.createElement("canvas");
-      // canvas.width = newImg.width;
-      // canvas.height = newImg.height;
-      // const context = canvas.getContext("2d");
-      // context.drawImage(newImg, 0, 0);
+      // Crear un canvas para mostrar las coordenadas de detección
+      const canvas = document.createElement("canvas");
+      canvas.width = newImg.width;
+      canvas.height = newImg.height;
+      const context = canvas.getContext("2d");
+      context.drawImage(newImg, 0, 0);
 
       
-      // // Dibujar las coordenadas de detección en el canvas
-      // items.forEach((element) => {
-      //   if (element.probability > 0.9) {
-      //     const nombre = element.tagName;
-      //     const probabilidad = element.probability;
-      //     const porcentaje = probabilidad * 100;
+      // Dibujar las coordenadas de detección en el canvas
+      items.forEach((element) => {
+        if (element.probability > 0.9) {
+          const nombre = element.tagName;
+          const probabilidad = element.probability;
+          const porcentaje = probabilidad * 100;
 
-      //     // Las coordenadas de detección generalmente se encuentran en element.boundingBox
-      //     const boundingBox = element.boundingBox;
+          // Las coordenadas de detección generalmente se encuentran en element.boundingBox
+          const boundingBox = element.boundingBox;
 
-      //     const originalImgWidth = newImg.naturalWidth;
-      //     const originalImgHeight = newImg.naturalHeight;
+          const originalImgWidth = newImg.naturalWidth;
+          const originalImgHeight = newImg.naturalHeight;
 
-      //     context.rect(
-      //       boundingBox.left * originalImgWidth,
-      //       boundingBox.top * originalImgHeight,
-      //       boundingBox.width * originalImgWidth,
-      //       boundingBox.height * originalImgHeight
-      //     );
-      //     context.lineWidth = 2;
-      //     context.strokeStyle = "red";
-      //     context.fillStyle = "red";
-      //     context.stroke();
+          context.rect(
+            boundingBox.left * originalImgWidth,
+            boundingBox.top * originalImgHeight,
+            boundingBox.width * originalImgWidth,
+            boundingBox.height * originalImgHeight
+          );
+          context.lineWidth = 2;
+          context.strokeStyle = "red";
+          context.fillStyle = "red";
+          context.stroke();
 
-      //     context.fillStyle = "red"; // Color y opacidad del fondo
-      //     context.fillRect(
-      //       boundingBox.left * originalImgWidth,
-      //       boundingBox.top * originalImgHeight - 20, // Ajustar la posición vertical del fondo
-      //       context.measureText(nombre).width + 65, // Ancho del fondo (basado en el texto)
-      //       20 // Altura del fondo
-      //     );
+          context.fillStyle = "red"; // Color y opacidad del fondo
+          context.fillRect(
+            boundingBox.left * originalImgWidth,
+            boundingBox.top * originalImgHeight - 20, // Ajustar la posición vertical del fondo
+            context.measureText(nombre).width + 65, // Ancho del fondo (basado en el texto)
+            20 // Altura del fondo
+          );
 
-      //     // Agregar etiqueta al cuadro de detección
-      //     context.font = "16px Arial";
-      //     context.fillStyle = "white";
-      //     context.fillText(
-      //       nombre,
-      //       boundingBox.left * originalImgWidth,
-      //       boundingBox.top * originalImgHeight + -5
-      //     );
+          // Agregar etiqueta al cuadro de detección
+          context.font = "16px Arial";
+          context.fillStyle = "white";
+          context.fillText(
+            nombre,
+            boundingBox.left * originalImgWidth,
+            boundingBox.top * originalImgHeight + -5
+          );
 
-      //     // Agregar otro comentario aquí
-      //     // Puedes personalizar el estilo del texto y su posición
-      //     context.font = "16px Arial";
-      //     context.fillStyle = "white";
-      //     context.fillText(
-      //       porcentaje.toFixed(0) + '%',
-      //       boundingBox.left * originalImgWidth + 52,
-      //       boundingBox.top * originalImgHeight - 5
-      //     );
-      //     console.log("Prediccion realizada con exito...");
-      //   }
-      // });
+          // Agregar otro comentario aquí
+          // Puedes personalizar el estilo del texto y su posición
+          context.font = "16px Arial";
+          context.fillStyle = "white";
+          context.fillText(
+            porcentaje.toFixed(0) + '%',
+            boundingBox.left * originalImgWidth + 52,
+            boundingBox.top * originalImgHeight - 5
+          );
+          console.log("Prediccion realizada con exito...");
+        }
+      });
 
-      // // Limpiar el contenido anterior y agregar la nueva imagen con las coordenadas de detección
-      // imagenContainer.innerHTML = '';
-      // imagenContainer.appendChild(canvas);
+      // Limpiar el contenido anterior y agregar la nueva imagen con las coordenadas de detección
+      imagenContainer.innerHTML = '';
+      imagenContainer.appendChild(canvas);
+
     })
     .catch((error) => {
       console.error(error);
